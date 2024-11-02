e1c85f81a68810b920bd530ce942071800475e9c
shifty-d <ffoxy2870@gmail.com>
2024-11-02 20:12:51 +0200

    tests

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -63,7 +63,7 @@ sed -i "215s/{config\['BuildKeys'\]}//" build/soong/scripts/gen_build_prop.py
 rm -rf vendor/qcom/opensource/commonsys/fm
 rm -rf vendor/qcom/opensource/power
 rm -rf vendor/qcom/opensource/usb
-git clone https://github.com/LineageOS/android_vendor_qcom_opensource_usb.git -b lineage-22.0 vendor/qcom/opensource/usb
+git clone https://github.com/halt-spesn/android_vendor_qcom_opensource_usb.git -b lineage-22.0 vendor/qcom/opensource/usb
 cd vendor/gms
 git lfs pull
 cd ../..
