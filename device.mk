68a966f255315fbf49b97453c11536dfb02ec69a
halt-spesn <ffoxy2870@gmail.com>
2024-11-14 18:55:25 +0000

    remove secondary camera pkg

--- device.mk
+++ device.mk
@@ -584,3 +584,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/refreshrate/privapp-permissions-refresh-rate-parts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-refresh-rate-parts.xml
+
+# RemovePackages
+PRODUCT_PACKAGES += \
+    RemovePackages
