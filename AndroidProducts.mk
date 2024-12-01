d48f6cff6c516b82cc449f9068a86a0c1b519892
Yograt <107334627+Yograt@users.noreply.github.com>
2024-12-01 19:08:16 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pixelage_spes.mk
+    $(LOCAL_DIR)/lineage_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    pixelage_spes-user \
-    pixelage_spes-userdebug \
-    pixelage_spes-eng
+    lineage_spes-user \
+    lineage_spes-userdebug \
+    lineage_spes-eng
