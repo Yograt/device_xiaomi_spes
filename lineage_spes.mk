e4a6d29ac6573202abc44503ae953e875ca5701d
Yograt <107334627+Yograt@users.noreply.github.com>
2024-12-01 19:44:24 +0700

    Update lineage_spes.mk
    Updated fingerprint

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -7,15 +7,19 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common Project Pixelage stuff.
-$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := false
 
 # Product Specifics
-PRODUCT_NAME := pixelage_spes
+PRODUCT_NAME := lineage_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -23,15 +27,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Boot Animaton
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Project Pixelage Flags
-PIXELAGE_BUILDTYPE := UNOFFICIAL
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Fingerprint
-BUILD_FINGERPRINT := "Redmi/spes/spes:13/TKQ1.221114.001/V14.0.6.0.TGKMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Redmi/spes/spes:13/TKQ1.221114.001/OS1.0.8.0.TGCMIXM:user/release-keys"
