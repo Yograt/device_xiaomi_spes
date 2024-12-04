30466aca8920ea1bb83e2aef928b4c9e4c3247e6
Yograt <107334627+Yograt@users.noreply.github.com>
2024-12-04 10:40:13 +0700

    Update lineage_spes.mk
    fixed issue with incomplete ")"

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
