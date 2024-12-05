19eee02159cd0bd9ab12e2a7e2e2015daa9fa7f9
HALt <102590462+halt-spesn@users.noreply.github.com>
2024-12-05 10:47:45 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -410,8 +410,6 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
-TARGET_PROVIDES_POWERHAL := true
-
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
