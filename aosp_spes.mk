7d49ab3994ed38492a8d1eefd9eb137533a801a9
Yograt <107334627+Yograt@users.noreply.github.com>
2024-12-08 16:07:04 +0700

    Update and rename euclid_spes.mk to aosp_spes.mk
    Updated for The Pixel Project

--- aosp_spes.mk
+++ aosp_spes.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common EuclidOS stuff.
-$(call inherit-product, vendor/euclid/config/common_full_phone.mk)
+# Inherit some common common stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := euclid_spes
+PRODUCT_NAME := aosp_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -26,13 +26,15 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Boot Animaton
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# EuclidOS Flags
-EUCLID_BUILD_TYPE := OFFICIAL
-EUCLID_MAINTAINER := тαиνir
-EUCLID_GAPPS := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_PIXEL_LAUNCHER := true
-TARGET_SUPPORTS_TOUCHGESTURES := true
+# The Pixel Project Flags
+CUSTOM_BUILD_TYPE := Unofficial
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_SUPPORTS_NOW_PLAYING := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+USE_PIXEL_CHARGER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_CLEAR_CALLING := true
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
