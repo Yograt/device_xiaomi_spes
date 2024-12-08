9029f0a3f23908885d973dc9c8e2f5f34fd94e6c
Yograt <107334627+Yograt@users.noreply.github.com>
2024-12-08 16:18:25 +0700

    Update aosp_spes.mk

--- aosp_spes.mk
+++ aosp_spes.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixel Project stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Product Specifics
 PRODUCT_NAME := aosp_spes
